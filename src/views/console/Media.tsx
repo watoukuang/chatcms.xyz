2078e9df607dfe14164700a54e8a93f2743fc0ad
smailsnail <651023907@qq.com>
2025-11-10 15:31:21 +0800

    ...

--- src/views/console/Media.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Media: React.FC = () => {
-  return (
-    <div>Platform Manager Content</div>
-  );
-};
-
-export default Media;
--- src/views/console/Prompt.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Prompt: React.FC = () => {
-  return (
-    <div>Prompt Manager Content</div>
-  );
-};
-
-export default Prompt;
