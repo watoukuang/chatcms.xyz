bbfe75e9f0092fd04870d81c4cc9be2908628c26
smailsnail <651023907@qq.com>
2025-11-15 22:28:02 +0800

    ...

--- layout/index.tsx
+++ layout/index.tsx
@@ -9,7 +9,7 @@ export default function Layout({children}: LayoutProps): React.ReactElement {
             className="flex flex-col min-h-screen bg-gradient-to-br from-green-50 via-green-50 to-green-100 dark:from-[#0f1115] dark:via-[#111317] dark:to-[#0f1115]">
             <Header/>
             <main className="flex-1 py-4 sm:py-6 lg:py-8">
-                <div className="container mx-auto px-6 sm:px-8 lg:px-12 max-w-7xl">
+                <div className="container mx-auto px-6 sm:px-8 lg:px-12">
                     {children}
                 </div>
             </main>
