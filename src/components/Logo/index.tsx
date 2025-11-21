d5d8b9e02a2bdff4b686040849a28a62c0d3a44d
smailsnail <651023907@qq.com>
2025-11-21 15:58:35 +0800

    修复整体页面布局

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -22,7 +22,7 @@ const Logo: React.FC<LogoProps> = ({className}) => {
                 className="shrink-0 inline-flex group-hover:brightness-110 group-hover:drop-shadow-[0_0_12px_var(--accent-shadow)]"
                 style={{color: 'var(--accent-color)'}}
             >
-                {/*<LogoIcon className="w-10 h-10 transition duration-200"/>*/}
+                <LogoIcon className="w-10 h-10 transition duration-200"/>
             </span>
             <span
                 className="truncate max-w-[40vw] sm:max-w-[50vw] md:max-w-none text-lg md:text-xl font-bold tracking-wide leading-none select-none drop-shadow-sm transition duration-200 filter group-hover:brightness-110 group-hover:drop-shadow-[0_0_12px_var(--accent-shadow)]"
